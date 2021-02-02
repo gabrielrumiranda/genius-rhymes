93108f150c07fe1a10fb3f41b547cc81705efd9a
gabrielrumiranda <gabrielrumiranda@gmail.com>
2021-02-02 13:11:38 -0300

    Finish refactor backend

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     devise_for :users
     resources :songs, only: [:create, :index, :show, :update]
-    resources :annotations, except: [:new, :edit]
+    resources :rhymes, only: [:create, :index, :show, :update, :destroy]
+    resources :syllables, only: [:create, :index, :show, :update, :destroy]
     resources :comments, only: [:create, :update, :destroy]
     post "votes", to: "votes#register"
   end
